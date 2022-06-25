b269d03a809bdbcb1e28d6789a6061faa7bf8d00
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-06-26 00:06:54 +0300

    feat: add code and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className='app-wrapper'>
+    <Header/>
+    <Profile/>
+    <Navbar/>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://dubaitrippackages.files.wordpress.com/2017/11/2-imgdinosaurs_base.jpg" alt="logo" />
+        </header>
+    )
+}
+
+export default Header

0d5824a760a3dafd2c3bddbb4b071b37cef08ed4
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-06-27 00:43:29 +0300

    feat: add classes and styles

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -15,11 +15,11 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className='posts'>
+                    <div className='item'>
                         post 1
                     </div>
-                    <div>
+                    <div className='item'>
                         post 2
                     </div>
                 </div>

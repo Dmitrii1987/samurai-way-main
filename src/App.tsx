a9b5ba71506e575b1f06860897845ef0b30c3f44
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-06-29 08:30:37 +0300

    fear: create Dialogues component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
+import Dialogues from './components/Dialogues/Dialogues';
 
 function App() {
   return (
     <div className='app-wrapper'>
     <Header/>
-    <Profile/>
     <Navbar/>
+    <Dialogues/>
+    {/* <Profile/> */}
     </div>
   );
 }
---
+++ src/components/Dialogues/Dialogues.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Dialogues.module.css'
+
+
+const Dialogues = () => {
+    return (
+        <div>
+            dialogues
+        </div>
+    );
+};
+
+export default Dialogues;

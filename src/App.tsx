64e2d06be01d9441cf37b74803484cc752ff8930
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-06-29 09:51:23 +0300

    refactor: fixed App.tsx with Routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,25 @@ import Navbar from './components/Navbar/Navbar';
 import Dialogues from './components/Dialogues/Dialogues';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <BrowserRouter>
-            <div className="wrapper">
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
-                <div className="app-wrapper-content">
+                <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path='/profile' element={<Profile/>}/>
-                        <Route path='/messages' element={<Dialogues/>}/>
+                        <Route path="/dialogs" element={<Dialogues/>}/>
+                        <Route path="/profile" element={<Profile/>}/>
+                        {/* <Route path="/news" element={<News/>}/>
+                        <Route path="/music" element={<Music/>}/>
+                        <Route path="/settings" element={<Settings/>}/> */}
                     </Routes>
                 </div>
             </div>
         </BrowserRouter>
-  );
+        );
 }
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+
+
+ReactDOM.render(<App />,  document.getElementById('root'));
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );

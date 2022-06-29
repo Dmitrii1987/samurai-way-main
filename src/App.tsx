eaf21805ad6e546671938cddf2e4d78c97ef8efb
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-06-29 09:14:31 +0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,22 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
 import Dialogues from './components/Dialogues/Dialogues';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-    <Header/>
-    <Navbar/>
-    <Dialogues/>
-    {/* <Profile/> */}
-    </div>
+    <BrowserRouter>
+            <div className="wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Routes>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/messages' element={<Dialogues/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
   );
 }
 

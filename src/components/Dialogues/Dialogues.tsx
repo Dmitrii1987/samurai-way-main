baddf4c0a9a58c148031029cf1aa42c60b5d347f
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-06-30 14:33:35 +0300

    feat: update Dialogues

--- src/components/Dialogues/Dialogues.tsx
+++ src/components/Dialogues/Dialogues.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogues.module.css'
 
 
 const Dialogues = () => {
     return (
-        <div>
-            dialogues
+        <div className={s.dialogues}>
+            <div className={s.dialoguesItems}>
+                <div className={s.dialogue}>
+                    <NavLink to={"/messages/1"}>
+                        Dima
+                    </NavLink>
+                </div>
+                <div className={s.dialogue}>
+                    <NavLink to={"/messages/2"}>
+                        Vika
+                    </NavLink>
+                </div>
+                <div className={s.dialogue}>
+                    <NavLink to={"/messages/3"}>
+                        Misha
+                    </NavLink>
+                </div>
+                <div className={s.dialogue}>
+                    <NavLink to={"/messages/4"}>
+                        Sasha
+                    </NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialogue}>
+                    Hello!!!
+                </div>
+                <div className={s.dialogue}>
+                    Give me Money!!!
+                </div>
+                <div className={s.dialogue}>
+                    I love u!
+                </div>
+            </div>
+
         </div>
     );
 };

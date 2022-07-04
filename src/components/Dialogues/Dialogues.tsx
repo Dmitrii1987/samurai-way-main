0a03b070c836bac1bd93be1ba3b96956099d7ab8
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-07-04 21:03:09 +0300

    refactor: change some folders

--- src/components/Dialogues/Dialogues.tsx
+++ src/components/Dialogues/Dialogues.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import DialogueItem from './DialogueItem';
+import DialogueItem from './DialogueItem/DialogueItem';
 import s from './Dialogues.module.css'
-import Message from './Message';
+import Message from './Message/Message';
 
 const Dialogues = () => {
+
 let dialogues = [
     {id:1, name:'Dima'},
     {id:2, name:'Vika'},
     {id:3, name:'Misha'},
     {id:4, name:'Sasha'}
 ]
+
 let messages = [
     {id:1, message:'Hello!'},
     {id:2, message:'Give me Money!!!'},
     {id:3, message:'I Love You'},
 ]
+
 let dialoguesElements = dialogues.map( d => <DialogueItem name={d.name} id={d.id} />);
 let messagesElements = messages.map(m => <Message dialogue={m.message} />)
 

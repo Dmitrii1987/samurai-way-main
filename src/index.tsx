f9fbed3d3fd6c6417f38536797716f5a317519e2
Dmitrii_K <dmitrii.kuzminyh@gmail.com>
2022-08-06 18:57:12 +0300

    feat:change index.ts and fix problem

--- src/index.tsx
+++ src/index.tsx
@@ -6,23 +6,26 @@
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
-import store,{ RootStateType} from "./Redux/store"
+import store, { RootStateType } from "./Redux/store"
 
-let rerenderEntireTree = (state:RootStateType)=> {
+let rerenderEntireTree = (state: RootStateType) => {
     ReactDOM.render(
         <BrowserRouter >
-            <App state={state} 
-            dispatch={store.dispatch.bind(store)} 
-            store={store}
+            <App state={state}
+                dispatch={store.dispatch.bind(store)}
+                store={store}
             // updateNewPostText={store.updateNewPostText.bind(store)} 
             />
         </BrowserRouter>, document.getElementById('root'));
-    
+
 }
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let state = store.getState()
+    rerenderEntireTree(state)
+});
 
 // ReactDOM.render(
 //   <React.StrictMode>

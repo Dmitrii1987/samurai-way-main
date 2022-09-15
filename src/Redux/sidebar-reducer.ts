625c0b56887edf9ebb3629b3f0bb1b3115a12371
Dmitrii_K <dmitrii.kuzminyh@gmail.com>
2022-09-15 14:31:27 +0300

    feat: add initialState in sidebar-reducer.ts

--- src/Redux/sidebar-reducer.ts
+++ src/Redux/sidebar-reducer.ts
@@ -1,6 +1,13 @@
 
+let initialState = {
+    friends: [
+        { id: 1, name: 'Sasha' },
+        { id: 2, name: 'Andrew' },
+        { id: 3, name: 'Michael' },
+    ]
+}
 
-const sidebarReducer = (state:any,action:any) => {
+const sidebarReducer = (state=initialState,action:any) => {
     return state
 }
 
